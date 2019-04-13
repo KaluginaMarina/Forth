0b2b92bd4e18281c7d6203f79cf60685754d827e
KaluginaMarina <marin.kalugin@gmail.com>
2019-04-14 01:05:42 +0300

    арифметические операции

--- functions.asm
+++ functions.asm
@@ -31,3 +31,76 @@ native swap, "swap"
 native dup, "dup"
 	push qword[rsp]
 	jmp next
+
+native plus, "+"
+	pop r10 
+	pop rax
+	add rax, r10
+	push rax
+	jmp next
+
+native minus, "-"
+	pop r10
+	pop rax
+	sub rax, r10
+	push rax
+	jmp next
+
+native mul, "*"
+	pop r10
+	pop rax
+	imul r10
+	push rax
+	jmp next
+
+native div, "/"
+	pop r10
+	pop rax
+	xor rdx, rdx
+	idiv r10
+	push rax
+	jmp next
+
+native mod, "%"
+	pop r10
+	pop rax
+	xor rdx, rdx
+	idiv r10
+	push rdx
+	jmp next
+
+native equals, "="
+	pop r10
+	pop r11
+	cmp r10, r11
+	je .equals
+	push 0
+	jmp next
+
+.equals:
+	push 1
+	jmp next
+
+native greater, "<"
+	pop r10
+	pop r11
+	cmp r10, r11
+	jg .greater
+	push 1
+	jmp next
+
+.greater:
+	push 0
+	jmp next
+
+native lesser, ">"
+	pop r10
+	pop r11
+	cmp r11, r10
+	jg .lesser
+	push 1
+	jmp next
+
+.lesser:
+	push 0
+	jmp next
